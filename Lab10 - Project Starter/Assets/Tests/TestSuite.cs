c39d3c1a832e8764660b72d3724f70ba46c1c1ac
33-RiveraMary <114891412+33-RiveraMary@users.noreply.github.com>
2022-10-31 15:53:54 +0800

    Setup NUnit and test folder

--- Lab10 - Project Starter/Assets/Tests/TestSuite.cs
+++ Lab10 - Project Starter/Assets/Tests/TestSuite.cs
@@ -8,21 +8,28 @@ namespace Tests
 {
     public class TestSuite
     {
+        private float result = 0.0f;
+
         // A Test behaves as an ordinary method
         [Test]
-        public void TestSuiteSimplePasses()
+        public void TestAddition()
         {
             // Use the Assert class to test conditions
+            result = Calculator.CalculatePair(5, 2, "+");
+            Assert.AreEqual(result, 7);
         }
 
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
         // `yield return null;` to skip a frame.
         [UnityTest]
-        public IEnumerator TestSuiteWithEnumeratorPasses()
+        public IEnumerator TestSuiteUnityAddition()
         {
             // Use the Assert class to test conditions.
             // Use yield to skip a frame.
             yield return null;
+
+            result = Calculator.CalculatePair(5, 2, "+");
+            Assert.AreEqual(result, 7);
         }
     }
 }
